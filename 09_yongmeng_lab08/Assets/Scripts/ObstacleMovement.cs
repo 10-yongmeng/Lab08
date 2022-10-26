8f0d5df716e4814ece4f231a36c54449a2b06a36
10_yongmeng <63108832+10-yongmeng@users.noreply.github.com>
2022-10-26 11:57:32 +0800

    game done

---
+++ 09_yongmeng_lab08/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    private float xSpeed = -4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
+        if (transform.position.y < -5)
+        {
+            Destroy(this.gameObject);
+            Player.score++;    
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            print("text");
+            SceneManager.LoadScene("GameLose");
+        }
+        if (other.gameObject.tag == "Wall")
+        {
+            Player.score++;
+        }
+
+    }
+}
---
+++ 09_yongmeng_lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public static float score;
+    public Text ScoreTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        if (transform.position.y >= 4f)
+        {
+            transform.position = new Vector3(-8.545298f, 4f, 0);
+        }
+        if (transform.position.y <= -4f)
+        {
+            transform.position = new Vector3(-8.545298f, -4f, 0);
+        }
+
+        ScoreTxt.text = "Score: " + score;  
+
+
+
+    }
+}
